6aa1a3c4c221c3744cf67c85e2a233d5b2716c46
Vitor <vitor@pop-os.localdomain>
2022-06-02 15:45:19 -0300

    Fetching my photo

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,25 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
 function App() {
-  return <div className="App"></div>;
+  const [user, setUser] = useState([]);
+
+  const getUser = () => {
+    axios.get("https://api.github.com/users/vit0rr").then((res) => {
+      const theUser = res.data;
+      setUser(theUser);
+    });
+  };
+
+  useEffect(() => getUser(), []);
+
+  if (!user) return <span>Loading...</span>
+  return (
+    <div className="App">
+      {console.log(user.avatar_url)}
+      <img src={user.avatar_url}></img>
+    </div>
+  );
 }
 
 export default App;
